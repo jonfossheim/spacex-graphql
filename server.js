e619060cfc64db734881e73b911a1c7f46c4638f
Jon Fossheim <fossheimjoneven@gmail.com>
2020-06-07 23:24:39 +0200

    update scripts

--- server.js
+++ server.js
@@ -29,6 +29,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-app.use('/.netlify/functions/api', router);
-
+module.exports = app;
 module.exports.handler = serverless(app);
