267246e2daf9905b58c705e0c1035b5b6995f930
Jon Fossheim <fossheimjoneven@gmail.com>
2020-06-07 23:15:23 +0200

    try netlify lambda

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
+
+const router = express.Router();
 
 app.use(cors());
 
@@ -18,10 +21,14 @@ app.use(
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.use('/.netlify/functions/api', router);
+
+module.exports.handler = serverless(app);
