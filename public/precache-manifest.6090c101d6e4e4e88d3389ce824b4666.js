501455dac976ed7c3de5f382c38ffec030c581b9
Jon Fossheim <fossheimjoneven@gmail.com>
2020-06-07 21:52:32 +0200

    testing

---
+++ public/precache-manifest.6090c101d6e4e4e88d3389ce824b4666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1bb100e885b7820d579116ad0c77b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7621bf2fa6e50b4478",
+    "url": "/static/css/main.0312f8cb.chunk.css"
+  },
+  {
+    "revision": "7c968ec035daa0334ae8",
+    "url": "/static/js/2.a7cae61f.chunk.js"
+  },
+  {
+    "revision": "6358e3a1edeb3407c864ac0519a42d91",
+    "url": "/static/js/2.a7cae61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7621bf2fa6e50b4478",
+    "url": "/static/js/main.957c9f36.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "dc45d3dff453115dbe0d23b384f34738",
+    "url": "/static/media/spacex_logo_black.dc45d3df.png"
+  }
+]);
